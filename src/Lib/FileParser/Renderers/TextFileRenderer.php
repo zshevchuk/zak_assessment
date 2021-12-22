c782d84438d1118eb706584bd86091a085c223cd
Zakharii Shevchuk <k1llforl1ve@yahoo.com>
2021-12-22 21:52:28 +0100

    Fix txt format

--- src/Lib/FileParser/Renderers/TextFileRenderer.php
+++ src/Lib/FileParser/Renderers/TextFileRenderer.php
@@ -3,17 +3,43 @@
 namespace App\Lib\FileParser\Renderers;
 
 use App\Lib\FileParser\Contracts\RendererInterface;
+use Symfony\Component\String\Inflector\EnglishInflector;
+use Symfony\Component\String\Inflector\InflectorInterface;
 
 class TextFileRenderer extends FileRenderer implements RendererInterface
 {
+    protected ?InflectorInterface $inflector = null;
+
     public function format(array $data): string
     {
         $output = '';
 
         foreach ($data as $key => $value) {
-            $output .= "$key : $value";
+
+            $output .= $this->createRow($key, $value);
         }
 
         return $output;
+    }
+
+    private function createRow(string $key, int|array $value)
+    {
+        $inflector = $this->getInflector();
+        if (is_array($value)) {
+            $inflector->pluralize($key);
+            $value = implode(",", $value);
+        }
+
+        return "$key : $value" . PHP_EOL;
+    }
+
+    private function getInflector()
+    {
+        if (null !== $this->inflector) {
+            return $this->inflector;
+        }
+
+        $this->inflector = new EnglishInflector();
+        return $this->inflector;
     }
 }
---
+++ src/Lib/FileParser/Renderers/TxtFileRenderer.php
@@ -0,0 +1,5 @@
+<?php
+
+namespace App\Lib\FileParser\Renderers;
+
+class TxtFileRenderer extends TextFileRenderer {}
