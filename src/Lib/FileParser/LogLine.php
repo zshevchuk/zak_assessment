8dc89824266536a3618c2582a1cdcdf2e30552f3
Zakharii Shevchuk <k1llforl1ve@yahoo.com>
2021-12-24 10:43:23 +0100

    remove redundant

--- src/Lib/FileParser/LogLine.php
+++ src/Lib/FileParser/LogLine.php
@@ -2,35 +2,14 @@
 
 namespace App\Lib\FileParser;
 
-use Symfony\Component\Validator\Constraints\NotBlank;
-use Symfony\Component\Validator\Mapping\ClassMetadata;
-use Symfony\Component\Validator\Constraints as Assert;
 use App\Lib\FileParser\Contracts\LineInterface;
 
-use Symfony\Component\Validator\Validator\ValidatorInterface;
-
-/**
-* @Assert\EnableAutoMapping()
-*/
 final class LogLine implements LineInterface
 {
-    /**
-     * @Assert\GreaterThan(50)
-     */
     public int $personId;
-    /**
-     * @Assert\GreaterThan(50)
-     */
     public string $bookId;
-    /**
-     * @Assert\GreaterThan(50)
-     */
     public string $timestamp;
-    /**
-     * @Assert\GreaterThan(50)
-     */
     public string $actionType;
-
 
     public function __construct(string $timestamp, int $personId, string $bookId, string $actionType)
     {
@@ -38,13 +17,5 @@ final class LogLine implements LineInterface
         $this->bookId = $bookId;
         $this->timestamp = strtotime($timestamp);
         $this->actionType = $actionType;
-    }
-
-    public static function loadValidatorMetadata(ClassMetadata $metadata)
-    {
-        $metadata->addPropertyConstraint(
-            'bookId',
-            new Assert\Length(['min' => 30])
-        );
     }
 }
